dd6ccbe7b8291b6cce5ac0497bda79495a2d9e9c
13298897828 <1120404010@qq.com>
2016-03-16 13:34:04 +0800

    添加视图

--- IWantFace/IWantFace/Classes/Scenes/ViewsConroller/StateViewController.swift
+++ IWantFace/IWantFace/Classes/Scenes/ViewsConroller/StateViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 let ImgHeight:CGFloat = 160
 typealias ChangeAplha = (alpha:Float) -> ()
 class StateViewController: UIViewController,UIScrollViewDelegate {
-
+    let Width = UIScreen.mainScreen().bounds.width
     @IBOutlet weak var scrolView: UIScrollView!
     @IBOutlet weak var topImgView: UIImageView!
     var backView:UIView = UIView()
     var backColor:UIColor = UIColor()
     var changeAplha:ChangeAplha?
+//    四项检测结果
+    @IBOutlet weak var numOfBlacks: UILabel!
+//    严重程度
+    @IBOutlet weak var severity: UILabel!
+//    毛孔
+    @IBOutlet weak var numOfPore: UILabel!
+//    光滑度
+    @IBOutlet weak var eggImgView: UIImageView!
+    @IBOutlet weak var questionMarkImgView: UIImageView!
+    
+    @IBOutlet weak var smoothDegreeOfSkin: UILabel!
+//   脸图片
+    @IBOutlet weak var faceImgView: UIImageView!
+//    三个围绕图
+    @IBOutlet weak var circleOfCheek: UIImageView!
+    @IBOutlet weak var circleOfChin: UIImageView!
+    @IBOutlet weak var circleOfT: UIImageView!
+//    统计图
+    @IBOutlet weak var statisticalImgView: UIImageView!
+    @IBOutlet weak var TLabel: UILabel!
+    @IBOutlet weak var cheekLabel: UILabel!
+    @IBOutlet weak var chinLabel: UILabel!
  
-    @IBOutlet weak var skinAgeLoop: SDPieLoopProgressView!
-    @IBOutlet weak var totalScore: SDPieLoopProgressView!
+    @IBOutlet weak var circle1: UIImageView!
+    @IBOutlet weak var circle2: UIImageView!
+    @IBOutlet weak var circle3: UIImageView!
+    @IBOutlet weak var circle4: UIImageView!
+    @IBOutlet weak var circle5: UIImageView!
+    @IBOutlet weak var circle6: UIImageView!
+    @IBOutlet weak var circle7: UIImageView!
+    @IBOutlet weak var circle8: UIImageView!
+ 
+    @IBOutlet weak var ageSkinLoop: SDLoopProgressView!
+    @IBOutlet weak var totalScoreLoop: SDLoopProgressView!
+    @IBOutlet weak var shareScrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var shareButton: UIButton!
+  
 //    用来拼接的分享视图
     var shareTopImg = UIImage()
     var shareBottomImg = UIImage()
@@ -30,24 +66,37 @@ class StateViewController: UIViewController,UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrolView.contentInset = UIEdgeInsetsMake(ImgHeight, 0, 0, 0)
-//      navigationBar变色
         backView.backgroundColor = backColor .colorWithAlphaComponent(0.3)
         backView = UIView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width, 41))
         backColor = UIColor.blackColor()
-        skinAgeLoop.tintColor = UIColor.redColor()
-        skinAgeLoop.progress = 0.1
- 
+        ageSkinLoop.progress = 0.6
+        shareScrollView.delegate = self
+        dealWithShareLabelText()
+        circleOfCheek.alpha = 0
+        addAnimationForCircle()
         
-
-//        分享文字
+    }
+    func addAnimationForCircle() {
+        UIView.transitionWithView(circleOfCheek, duration: 2, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+             self.circleOfCheek.alpha = 1
+            }) { (Bool) -> Void in
+                
+        }
+        
+        
+    }
+    
+// MARK: -   处理分享文字样式
+    func dealWithShareLabelText() {
+        //        分享文字
         var text = String()
         text = "你的肌肤光滑度超过了全杭州71%的女生,综合得分排名靠前"
         let text3 = NSMutableAttributedString(string: text)
-//        改颜色
+        //        改颜色
         let needle: Character = "%"
         if let idx = text.characters.indexOf(needle) {
             let pos = text.startIndex.distanceTo(idx)
-//            print("Found \(needle) at position \(pos)")
+            //            print("Found \(needle) at position \(pos)")
             text3.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 254 / 255.0, green: 220 / 255.0, blue: 98 / 255.0, alpha: 1), range: NSMakeRange(pos - 2, 3))
             print(pos,idx)
             let style = NSMutableParagraphStyle()
@@ -59,6 +108,7 @@ class StateViewController: UIViewController,UIScrollViewDelegate {
         else {
             print("Not found")
         }
+        
     }
     func scrollViewDidScroll(scrollView: UIScrollView) {
         
@@ -88,9 +138,14 @@ class StateViewController: UIViewController,UIScrollViewDelegate {
 //  // MARK: - pageControl
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         
-//        pageControl.currentPage =  Int(sharedScrollView.contentOffset.x / Width)
+        pageControl.currentPage =  Int(shareScrollView.contentOffset.x / Width)
         
     }
+  
+    @IBAction func shareAction(sender: UIButton) {
+        print(shareScrollView.contentOffset.x)
+    }
+    
     override func viewDidAppear(animated: Bool) {
         let con =  self.view.superview!.superview!.superview!.superview!.superview!.superview!.nextResponder()! as!ContainViewController
         con.navigationController?.navigationBar.addSubview(backView)
@@ -106,6 +161,8 @@ class StateViewController: UIViewController,UIScrollViewDelegate {
   
         
     }
+    
+    
  
 //    // MARK: -  截屏幕
     func getImage() -> (UIImage){
--- IWantFace/IWantFace/Classes/Vendors/loop/SDLoopProgressView.m
+++ IWantFace/IWantFace/Classes/Vendors/loop/SDLoopProgressView.m
@@ -16,20 +16,28 @@
     
     CGFloat xCenter = rect.size.width * 0.5;
     CGFloat yCenter = rect.size.height * 0.5;
-    [[UIColor whiteColor] set];
-    
-    CGContextSetLineWidth(ctx, 15 * SDProgressViewFontScale);
+//    [[UIColor whiteColor] set];
+    [SDColorMaker(178, 199, 194, 1) set];
+//    [SDColorMaker(47, 47, 47, 1) set];    黑
+    CGContextSetLineWidth(ctx, 7 * SDProgressViewFontScale);
     CGContextSetLineCap(ctx, kCGLineCapRound);
     CGFloat to = - M_PI * 0.5 + self.progress * M_PI * 2 + 0.05; // 初始值0.05
     CGFloat radius = MIN(rect.size.width, rect.size.height) * 0.5 - SDProgressViewItemMargin;
     CGContextAddArc(ctx, xCenter, yCenter, radius, - M_PI * 0.5, to, 0);
     CGContextStrokePath(ctx);
+    [SDColorMaker(47, 47, 47, 1) set];
+    CGFloat maskW = (radius - 15) * 2.2;
+    CGFloat maskH = maskW;
+    CGFloat maskX = (rect.size.width - maskW) * 0.5;
+    CGFloat maskY = (rect.size.height - maskH) * 0.5;
+    CGContextAddEllipseInRect(ctx, CGRectMake(maskX, maskY, maskW, maskH));
+    CGContextFillPath(ctx);
     
     // 进度数字
     NSString *progressStr = [NSString stringWithFormat:@"%.0f", self.progress * 100];
     NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
     attributes[NSFontAttributeName] = [UIFont boldSystemFontOfSize:20 * SDProgressViewFontScale];
-    attributes[NSForegroundColorAttributeName] = [UIColor lightGrayColor];
+    attributes[NSForegroundColorAttributeName] = [UIColor whiteColor];
     [self setCenterProgressText:progressStr withAttributes:attributes];
 }
 
